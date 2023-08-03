997de5c8ce8c7e1789701f9d01d50e53f115e41d
seeyoo <lukdown10@gmail.com>
2023-08-03 16:21:04 +0900

    ,.,.

--- sci_learn_03/times_bank.py
+++ sci_learn_03/times_bank.py
@@ -3,5 +3,11 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 df = pd.read_csv('data/banklist.csv', encoding='utf-8')
-print(df.head())
-print(df.columns)
+df['close_dt'] = pd.to_datetime(df['Closing Date'])
+df['update_dt'] = pd.to_datetime(df['Updated Date'])
+df['close_year'] = df['close_dt'].dt.year   # 연도만 가지고 그룹핑
+# 그룹핑하면 새롭게 Series 데이터 생성
+sr_closing_year = df.groupby(['close_year']).size()
+# print(df.head())
+# df.info()
+print(sr_closing_year)
