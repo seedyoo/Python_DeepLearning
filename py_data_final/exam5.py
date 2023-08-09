8e7c2f93027339359ce2370060f58fa16a6d836f
seeyoo <lukdown10@gmail.com>
2023-08-09 16:24:20 +0900

    ...

--- py_data_final/exam5.py
+++ py_data_final/exam5.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('datago/201212_201712_연령별인구현황_연간.csv', encoding='cp949')
+df = pd.read_csv('datago/201212_201412_연령별인구현황_연간.csv', encoding='cp949')
 
-print(df.iloc[1,0])
+for r in range(1, len(df)):
+    print(df.iloc[r,0])
+    for c in range(len(df.columns)):
+        if df.columns[c].endswith('년_거주자_30~39세'):
+            print(df.iloc[r,c])
 
-for c in range(len(df.columns)):
-    df.columns[c]
+# 2017년에 인구수가 제일 많은 행정구역은 어디인가요?
